c7ce2b14ed0d113bf5641e8e01d6db539f863c0d
LEO <leo@ubuntu.(none)>
2013-09-17 17:12:47 -0500

    Wuu

--- Atomica.py
+++ Atomica.py
@@ -22,7 +22,7 @@ def y2_prime(u,y1,y2,E,l):
 # Definimos el intervalo [a,b] donde queremos resolver nuestra ecuacion diferencial y definimos el paso h que vamos a tomar
 
 a = 0.0
-b = 6.0
+b = 10.0
 h = 0.1
 
 n_points = int((b-a)/h)
@@ -30,7 +30,7 @@ u = [a+i*h for i in range(1,n_points+1)]
 
 for n in range(1,4):
      #E = -1.0/float(n**2)
-     E= -1.0/9
+     E= -1.0/4.0
      L = [l*(l+1) for l in range(n)]
      for l in range(len(L)):
           print 'solving for: n = '+str(n)+', l = '+str(l)
@@ -39,6 +39,8 @@ for n in range(1,4):
 	  y3 = []
 	  integrate=0.0	
 	  y4=[]
+	  ymax=0.0
+	  umax=0.0
           if l == 0 :
                y1.append(1.0)
                y2.append(1.0)
@@ -62,12 +64,10 @@ for n in range(1,4):
           print integrate
           for i in range(1,n_points):
 
-	       y4.append(y3[i-1]/integrate)
+	       y4.append((y3[i-1])/integrate)
 	       
-	
-	
-       
-	
+	  print 'Max: '+ str(max(y4)) + ' en pos ' + str(u[y4.index(max(y4))])
+
           pylab.plot(u , y4, 'ko')
          # pylab.xlim([0,b])
           pylab.xlabel('$u$')
@@ -76,5 +76,13 @@ for n in range(1,4):
           pylab.savefig(str(n)+','+str(l)+'.png')
           pylab.close()
 
-for l in range(len(y4)):
-	print l
+print ymax
+
+
+
+	
+		
+
+	
+	
+	
